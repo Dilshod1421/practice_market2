4df32b5a16ca5b944f14dd08b5b1f8970d24e6d4
Dilshod1421 <dilshod7861@gmail.com>
2023-01-22 17:16:05 +0500

    login done

--- server.js
+++ server.js
@@ -1,14 +1,29 @@
 const http = require('http');
+const jwt = require('jsonwebtoken');
 const { read_file, write_file } = require('./api/fsRead');
 require('dotenv').config();
 const port = process.env.PORT || 6789;
+const key_jwt = process.env.SECRET_KEY
 const options = { "Content-Type": "application/json" };
 
 const server = http.createServer((req, res) => {
+    let admin = read_file('admin.json');
     res.writeHead(200, options);
+    if (req.method === 'POST') {
+        if (req.url === '/login') {
+            req.on('data', (data) => {
+                data = JSON.parse(data);
+                if (admin[0].name != data.name || admin[0].password != data.password) {
+                    return res.end(JSON.stringify("Wrong name or password"));
+                };
+                let token = jwt.sign({ ...data }, key_jwt, { expiresIn: '10h' });
+                res.end(JSON.stringify(token));
+            });
+        }
+    }
 });
 
 server.listen(port, (err) => {
     if (err) throw err;
-    console.log('server listening on port' + port);
+    console.log('server listening on port ' + port);
 })
